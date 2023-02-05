7bb80aa5a688da1a258bb8f775835f2090e225c6
candle <candle@one.com>
2023-02-05 15:26:30 +0800

    finish thread lab

--- notxv6/barrier.c
+++ notxv6/barrier.c
@@ -30,7 +30,21 @@ barrier()
   // Block until all threads have called barrier() and
   // then increment bstate.round.
   //
-  
+  pthread_mutex_lock(&bstate.barrier_mutex);
+  bstate.nthread++;
+  if(bstate.nthread == nthread){
+     //表示所有线程已经到达
+     bstate.nthread=0;
+     bstate.round++;
+     pthread_cond_broadcast(&bstate.barrier_cond);
+     //bstate.nthread=0;
+
+  }
+  else{
+	  //表示
+    pthread_cond_wait(&bstate.barrier_cond,&bstate.barrier_mutex);
+  }
+  pthread_mutex_unlock(&bstate.barrier_mutex);
 }
 
 static void *
